a0dbc50466ec08dfd4a619d05e5308e2e25badf1
Haebun <mediafactory1279@gmail.com>
2021-07-22 02:35:30 +0900

    게시물 작성 레이아웃 디자인 및 작성 취소 시 알림창 기능 구현(AlertDialog)

--- app/src/main/java/com/wisappstudio/hobbing/activity/WritePostActivity.java
+++ app/src/main/java/com/wisappstudio/hobbing/activity/WritePostActivity.java
@@ -1,8 +1,11 @@
 package com.wisappstudio.hobbing.activity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.hobbing.R;
@@ -14,4 +17,25 @@ public class WritePostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_post);
     }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("게시물 작성 취소");
+        builder.setMessage("작성중이던 게시물을 취소하시겠습니까?\r\n취소 시 작성된 내용은 저장되지 않습니다.");
+        builder.setPositiveButton("작성 취소",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(),"작성을 취소했습니다.",Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                });
+        builder.setNegativeButton("계속 쓰기",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) { }
+                });
+
+        builder.show();
+    }
+
 }
--- app/src/main/java/com/wisappstudio/hobbing/fragment/MainPageFragment.java
+++ app/src/main/java/com/wisappstudio/hobbing/fragment/MainPageFragment.java
@@ -29,9 +29,6 @@ public class MainPageFragment extends Fragment {
         fab.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
                 Intent intent = new Intent(view.getContext(), WritePostActivity.class);
                 startActivity(intent);
             }
