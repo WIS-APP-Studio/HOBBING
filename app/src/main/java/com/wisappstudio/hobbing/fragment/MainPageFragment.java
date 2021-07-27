e6d1b3bd9f758c68b74e949ad9f5852e8e15a4ca
Haebun <mediafactory1279@gmail.com>
2021-07-28 04:52:53 +0900

    내부 게시물 댓글창 레이아웃 구현(No DB Connection)

--- app/src/main/java/com/wisappstudio/hobbing/fragment/MainPageFragment.java
+++ app/src/main/java/com/wisappstudio/hobbing/fragment/MainPageFragment.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
