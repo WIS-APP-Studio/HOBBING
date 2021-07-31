11e721c8d7097f6b573aa473a1595c0f40f07036
JHmins <jadejohn@naver.com>
2021-07-31 14:43:45 +0900

    1차 업로드

--- app/src/main/java/com/wisappstudio/hobbing/data/ServerData.java
+++ app/src/main/java/com/wisappstudio/hobbing/data/ServerData.java
@@ -3,7 +3,7 @@ package com.wisappstudio.hobbing.data;
 public class ServerData {
     public static String IP = "182.228.83.7";
     public static String LOCALHOST = "192.168.219.110";
-    public static String URL = "http://"+LOCALHOST+"/hobbing/";
+    public static String URL = "http://"+IP+"/hobbing/";
     public static String SIGN_UP_URL = URL+"login/sign_up.php";
     public static String SIGN_IN_URL = URL+"login/sign_in.php";
     public static String POST_READ_URL = URL+"post/read.php";
--- app/src/main/java/com/wisappstudio/hobbing/fragment/ServicePageFragment.java
+++ app/src/main/java/com/wisappstudio/hobbing/fragment/ServicePageFragment.java
@@ -4,24 +4,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.hobbing.R;
 
 public class ServicePageFragment extends Fragment {
+    static final String[] LIST_MENU = {"내 정보", "알림 설정", "활동 기록", "로그아웃", "탈퇴"} ;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         setHasOptionsMenu(true);
+
+        View rootView = inflater.inflate(R.layout.activity_service_page, null);
+
+        ArrayAdapter Adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, LIST_MENU) ;
+
+        ListView listview = (ListView) rootView.findViewById(R.id.listview1) ;
+        listview.setAdapter(Adapter) ;
+
         return inflater.inflate(R.layout.activity_service_page, container, false);
+
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
+
 
 }
