ef608b77bcb35045d00ad27919f99d15ad674bc0
Haebun <mediafactory1279@gmail.com>
2021-08-04 00:58:23 +0900

    게시물 댓글 작성 기능 구현(DB Connection) 및 댓글 목록 이중 스크롤 제거(UX 개선)

--- app/src/main/java/com/wisappstudio/hobbing/activity/InnerPostActivity.java
+++ app/src/main/java/com/wisappstudio/hobbing/activity/InnerPostActivity.java
@@ -3,7 +3,11 @@ package com.wisappstudio.hobbing.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,10 +30,8 @@ import com.bumptech.glide.signature.ObjectKey;
 import com.example.hobbing.R;
 import com.wisappstudio.hobbing.adapter.CommentAdapter;
 import com.wisappstudio.hobbing.adapter.InnerPostAdapter;
-import com.wisappstudio.hobbing.adapter.PostAdapter;
 import com.wisappstudio.hobbing.data.CommentData;
 import com.wisappstudio.hobbing.data.InnerPostData;
-import com.wisappstudio.hobbing.data.PostData;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -42,11 +44,14 @@ import java.util.Map;
 import static com.wisappstudio.hobbing.data.ServerData.INNER_POST_COMMENT_READ_URL;
 import static com.wisappstudio.hobbing.data.ServerData.INNER_POST_IMAGE_READ_URL;
 import static com.wisappstudio.hobbing.data.ServerData.INNER_POST_READ_URL;
+import static com.wisappstudio.hobbing.data.ServerData.INNER_POST_SEND_COMMENT_URL;
 import static com.wisappstudio.hobbing.data.ServerData.PROFILE_IMAGE_DIRECTORY;
 
 public class InnerPostActivity extends AppCompatActivity {
     ArrayList<InnerPostData> innerPostDataList;
     ArrayList<CommentData> commentDataList;
+    String userId;
+    String postNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +60,12 @@ public class InnerPostActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String userId = intent.getStringExtra("user_id");
+        String postNumber = intent.getStringExtra("number");
+        this.userId = userId;
+        this.postNumber = postNumber;
 
+        // 댓글 사용자 프로필 사진
         ImageView userProfile = (ImageView) findViewById(R.id.list_inner_post_user_profile);
-
         Glide.with(getApplicationContext())
                 .load(PROFILE_IMAGE_DIRECTORY +userId+".png") // 임시로 로드
                 .apply(new RequestOptions()
@@ -68,6 +76,8 @@ public class InnerPostActivity extends AppCompatActivity {
                 .into(userProfile);
 
         RequestQueue queue = Volley.newRequestQueue(this);
+
+
         // 게시물 내부 내용 로드
         StringRequest strRequest = new StringRequest(Request.Method.POST, INNER_POST_READ_URL, new Response.Listener<String>() {
             @Override
@@ -132,11 +142,12 @@ public class InnerPostActivity extends AppCompatActivity {
                 try {
                     JSONObject jsonObject = new JSONObject(response);
                     InitializeCommentData(jsonObject);
-
                     ListView commentListView = (ListView) findViewById(R.id.activity_inner_post_comment);
+
                     final CommentAdapter adapter = new CommentAdapter(getApplicationContext(), commentDataList);
 
                     commentListView.setAdapter(adapter);
+                    setListViewHeightBasedOnChildren(commentListView);
                     //
                 } catch (JSONException e) {
                     Log.d("COMMENTERR", e.getMessage());
@@ -156,6 +167,51 @@ public class InnerPostActivity extends AppCompatActivity {
                 return params;
             }
         };
+
+        // 댓글 작성 및 전송
+        ImageView sendComment = (ImageView) findViewById(R.id.activity_inner_post_send_comment);
+
+        sendComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = getIntent();
+                String userId = intent.getStringExtra("user_id");
+                String postNumber = intent.getStringExtra("number");
+
+                EditText writtenComment = (EditText) findViewById(R.id.activity_inner_post_comment_description);
+                String commentDescription = writtenComment.getText().toString();
+
+                if(commentDescription.length() == 0) {
+                    Toast.makeText(InnerPostActivity.this, "먼저 댓글을 입력해주세요!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                writtenComment.setText("");
+
+                StringRequest sendCommentRequest = new StringRequest(Request.Method.POST, INNER_POST_SEND_COMMENT_URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(InnerPostActivity.this, "댓글을 작성했습니다.", Toast.LENGTH_SHORT).show();
+                        queue.add(commentRequest);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(InnerPostActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                        // 댓글 작성한 내용 전송 중 오류로 서비스가 되지 않은 경우를 보여줘야함 Exception Process
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("user_id", userId);
+                        params.put("post_number", postNumber);
+                        params.put("comment", commentDescription);
+                        return params;
+                    }
+                };
+                queue.add(sendCommentRequest);
+            }
+        });
 
         queue.add(strRequest);
         queue.add(stringRequest);
@@ -196,13 +252,13 @@ public class InnerPostActivity extends AppCompatActivity {
             TextView ownerView = findViewById(R.id.activity_inner_post_owner);
             TextView descriptionView = findViewById(R.id.activity_inner_post_description);
             TextView titleView = findViewById(R.id.activity_inner_post_title);
+            ImageView select = (ImageView) findViewById(R.id.activity_inner_post_select);
 
             ownerView.setText(writer + "님의 게시물");
             descriptionView.setText(description);
             titleView.setText(title);
 
         } catch (JSONException e) {
-            Log.d("LoadERR-JSONException", e.toString());
         }
     }
 
@@ -248,4 +304,27 @@ public class InnerPostActivity extends AppCompatActivity {
 
         } catch (JSONException e) { e.printStackTrace(); }
     }
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.AT_MOST);
+
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+    }
+
 }
