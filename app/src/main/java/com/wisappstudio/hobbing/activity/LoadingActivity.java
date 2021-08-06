3463d1c56668d7aa7bae3135a9346a4429a4f212
Hoon <kyunghoon13@gmail.com>
2021-08-07 01:29:41 +0900

    1차 업로드

--- app/src/main/java/com/wisappstudio/hobbing/activity/LoadingActivity.java
+++ app/src/main/java/com/wisappstudio/hobbing/activity/LoadingActivity.java
@@ -1,5 +1,6 @@
 package com.wisappstudio.hobbing.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,21 +12,29 @@ import com.example.hobbing.R;
 public class LoadingActivity extends AppCompatActivity {
     private int LoadingTime = 3000; // 밀리세컨드 단위 ( 1000(milli second) = 1(second)
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
+        ProgressDialog progressDialog = new ProgressDialog(this,R.style.AppCompatAlertDialogStyle);
+        progressDialog.setMessage("테스트");
+        progressDialog.show();
+
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+                progressDialog.dismiss();
                 Intent intent = new Intent(getApplicationContext(), IntroActivity.class);
                 startActivity(intent);
                 finish();
+
             }
         }, LoadingTime);
     }
+
 
     @Override
     protected void onPause() {
